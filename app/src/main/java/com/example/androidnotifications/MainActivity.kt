57e7c7969287baee776976595fe454cab38c5ef6
AndrewZamora <andrewmichaelzamora@gmail.com>
2021-02-03 20:49:45 +0900

    Added createRegularNotification function and added button to trigger it

--- app/src/main/java/com/example/androidnotifications/MainActivity.kt
+++ app/src/main/java/com/example/androidnotifications/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.androidnotifications
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.app.NotificationCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 //    https://developer.android.com/training/notify-user/build-notification
@@ -13,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         createNotificationChannel()
+        findViewById<Button>(R.id.button_regular).setOnClickListener{ view ->
+            createRegularNotification("Regular Notification Title","Regular Notification Text", "Default")
+        }
     }
 
     private fun createNotificationChannel() {
@@ -20,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         val descriptionText = "Default description text"
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel("0", name, importance).apply {
+            val channel = NotificationChannel("Default", name, importance).apply {
                 description = descriptionText
             }
             val notificationManager: NotificationManager =
@@ -29,9 +37,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun createStandardNotification(contentTitle: String, contentText: String, channelId: String) {
+    private fun createRegularNotification(contentTitle: String, contentText: String, channelId: String) {
 //        https://material.io/design/platform-guidance/android-notifications.html#anatomy-of-a-notification
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(contentText)
+                .setContentText(contentTitle)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
 
+        // Not the best way to create a unique ID but a unique ID is needed to prevent the last notification from being overwritten
+        val uniqueId = Random(System.currentTimeMillis()).nextInt(1000)
+        notificationManager.notify(uniqueId, notificationBuilder.build())
     }
 }
